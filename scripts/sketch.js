943e4f7249ddda02cc7e9a01dfe219b2fecb6e7e
yeseuljoo <dptmfe93@naver.com>
2020-12-21 17:19:40 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -4,15 +4,26 @@ function setup() {
   createCanvas(540, 540);
   img = loadImage('../assets/people.jpg');
   
-  // Create a request variable and assign a new XMLHttpRequest object to it.
-  var request = new XMLHttpRequest()
-
-  // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET', 'https://cv-api.kakaobrain.com/pose', true)
-
-  request.onload = function () {
-    console.log(request.status);
-  }
+  function postRequest() { 
+   
+  // Do a POST request to the test API 
+  let api_url = 'https://reqres.in/api/users'; 
+   
+  // Example POST data 
+  let postData = { id: 1, name: "Sam", 
+                  email: "sam@samcorp.com" }; 
+   
+  httpPost(api_url, 'json', postData, function (response) { 
+    text("Data returned from API", 20, 100); 
+   
+    text("The ID in the data is: "
+         + response.id, 20, 140); 
+    text("The Name in the data is: " 
+         + response.name, 20, 160); 
+    text("The Email in the data is: " 
+         + response.email, 20, 180); 
+  }); 
+} 
 }
 
 function draw() {
