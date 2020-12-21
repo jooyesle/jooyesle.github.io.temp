2dce8cb1f717f5aeefb5b41e4ef6f86165059f66
yeseuljoo <dptmfe93@naver.com>
2020-12-21 17:52:37 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -13,7 +13,7 @@ function preload() {
       method: 'POST',
       // Other Request options, like special headers for apis
       headers: { 'Content-Type': 'multipart/form-data',
-                 'Authorization: KakaoAK 687ea12e4ef2be02334d085696877d60'
+                 'Authorization': 'KakaoAK 687ea12e4ef2be02334d085696877d60'
                },
       form: {
                 'file'='@people.jpg'
