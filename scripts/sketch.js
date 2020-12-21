6f491600676b7eadab1f6b3524779878cbf8809a
yeseuljoo <dptmfe93@naver.com>
2020-12-21 17:41:42 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -3,22 +3,28 @@ let img;
 function setup() {
   createCanvas(540, 540);
   img = loadImage('../assets/people.jpg');
-  
-  function postRequest() { 
-   
-  // Do a POST request to the test API 
-  let api_url = 'https://cv-api.kakaobrain.com/pose/job'; 
-   
-  // Example POST data 
-  let postData = { Content-Type: multipart/form-data,
-                  Authorization: KakaoAK 687ea12e4ef2be02334d085696877d60, 
-                  file: ../assets/people.jpg }; 
-   
-  httpPost(api_url, 'json', postData, function (response) { 
-    console.log(response.area); 
-  }); 
-} 
 }
+
+function preload() {
+  let url = 'https://cv-api.kakaobrain.com/pose';
+  httpDo(
+    url,
+    {
+      method: 'POST',
+      // Other Request options, like special headers for apis
+      headers: { Content-Type: multipart/form-data,
+                 Authorization: KakaoAK 687ea12e4ef2be02334d085696877d60
+               },
+      form: {
+                file=@people.jpg
+            }
+    },
+    function(res) {
+      
+    }
+  );
+}
+
 
 function draw() {
     //image(img, 0, 0);
