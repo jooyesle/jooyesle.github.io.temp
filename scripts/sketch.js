04a0dc684fa4a01c5163c8410129a9105a116946
yeseuljoo <dptmfe93@naver.com>
2020-12-22 10:35:35 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -9,11 +9,8 @@ function preload() {
   let request = new XMLHttpRequest();
   let url = 'https://cv-api.kakaobrain.com/pose';
   
-  let formData = new FormData();
-  
-  
-  
   request.open('POST', url, true);
+  request.setRequestHeader('Access-Control-Allow-Origin', '*');
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   request.setRequestHeader('Authorization', 'KakaoAK 687ea12e4ef2be02334d085696877d60');
   request.send('image_url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FLXwvSAzEfmQ%2Fmaxresdefault.jpg');
