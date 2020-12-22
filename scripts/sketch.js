87e6c4d4bf6cbb8a0688a7d2c6f9c0a9cf88219f
yeseuljoo <dptmfe93@naver.com>
2020-12-22 11:42:26 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -7,16 +7,17 @@ function setup() {
 
 let request = new XMLHttpRequest();
 
-/*function preload() {
+function preload() {
   let url = 'https://cv-api.kakaobrain.com/pose';
   
   request.open('POST', url, true);
   request.setRequestHeader('Access-Control-Allow-Origin', '*');
+  request.setRequestHeader('Access-Control-Allow-Methods', 'POST'); 
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   request.setRequestHeader('Authorization', 'KakaoAK 687ea12e4ef2be02334d085696877d60');
   request.send('image_url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FLXwvSAzEfmQ%2Fmaxresdefault.jpg');
   
-}*/
+}
 
 
 function draw() {
