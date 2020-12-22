a8933ac15b8db9a0d0b0cd290724c8b3807473d1
yeseuljoo <dptmfe93@naver.com>
2020-12-22 11:26:13 +0900

    Update pose.py

--- python/pose.py
+++ python/pose.py
@@ -1,4 +1,7 @@
 import requests
+
+html_file = open('../_layouts/default.html','w')
+
 APP_KEY = '687ea12e4ef2be02334d085696877d60'
 # IMAGE_URL = 'http://example.com/example.jpg'
 IMAGE_FILE_PATH = '../assets/people.jpg'
@@ -13,3 +16,11 @@ session.headers.update({'Authorization': 'KakaoAK ' + APP_KEY})
 with open(IMAGE_FILE_PATH, 'rb') as f:
     response = session.post('https://cv-api.kakaobrain.com/pose', files=[('file', f)])
     print(response.status_code, response.json())
+    
+ data = ''
+ for i in a: #TIP: use a generator
+     scope['x']+=1
+     data += a[x]
+     data += '\n'
+ html_file.write(data)
+ html_file.close()
