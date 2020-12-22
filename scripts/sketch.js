a113ca89b0ec40ecebfb06b3733a78f1d2714449
yeseuljoo <dptmfe93@naver.com>
2020-12-22 13:45:37 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -13,9 +13,9 @@ function preload() {
   request.open('POST', url, true);
   request.setRequestHeader('Access-Control-Allow-Origin', '*');
   request.setRequestHeader('Access-Control-Allow-Methods', 'POST'); 
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  request.setRequestHeader('Content-Type', 'multipart/form-data');
   request.setRequestHeader('Authorization', 'KakaoAK 687ea12e4ef2be02334d085696877d60');
-  request.send('image_url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FLXwvSAzEfmQ%2Fmaxresdefault.jpg');
+  request.send('file=@../assets/people.jpg');
   
 }
 
