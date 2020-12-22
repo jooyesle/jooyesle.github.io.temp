6a93393605f28612f2dc0448af3512755685bc4a
yeseuljoo <dptmfe93@naver.com>
2020-12-22 09:54:13 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -6,24 +6,13 @@ function setup() {
 }
 
 function preload() {
+  let request = new XMLHttpRequest();
   let url = 'https://cv-api.kakaobrain.com/pose';
-  httpDo(
-    url,
-    {
-      method: 'POST',
-      // Other Request options, like special headers for apis
-      headers: { 
-                 'Content-Type': 'multipart/form-data',
-                 'Authorization': 'KakaoAK 687ea12e4ef2be02334d085696877d60'
-               },
-      form: {
-                'file':'@../assets/people.jpg'
-            }
-    },
-    function(res) {
-      
-    }
-  );
+  request.open('POST', url, true);
+  
+  request.setRequestHeader('Content-Type', 'multipart/form-data');
+  request.setRequestHeader('Authorization', 'KakaoAK 687ea12e4ef2be02334d085696877d60');
+
 }
 
 
