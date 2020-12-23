a386825d6be2c8d5d9daa89d7f8ff442d189225c
yeseuljoo <dptmfe93@naver.com>
2020-12-23 23:44:46 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -74,9 +74,9 @@ function mousePressed() {
   req.open('POST', 'https://api.imgbb.com/1/upload?expiration=600&key=15c781598b3e34982799db6f86a3819f', true);
   req.setRequestHeader('Access-Control-Allow-Origin', '*');
   req.setRequestHeader('Access-Control-Allow-Methods', 'POST'); 
-  req.send('image='+ canvas.toDataURL("image/png").split(';base64,')[1]);
-
-  console.log(canvas.toDataURL("image/png").split(';base64,')[1]);
+  //req.send('image='+ canvas.toDataURL("image/png").split(';base64,')[1]);
+  req.send('image=R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+  console.log(canvas.toDataURL());
   
   req.onload = function() {
     console.log(req.response);
