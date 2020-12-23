a4fc6e3b8d5dbcff7434f0f8d9125062ecc470bc
yeseuljoo <dptmfe93@naver.com>
2020-12-23 10:24:51 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -25,18 +25,17 @@ function preload() {
     console.log(request.response);
     let result = JSON.parse(request.response);
     
-    for(let keykey in result){
-      let key = parseInt(keykey);
+    for(let key in result){
       console.log(result[key].keypoints);
       
       fill(random(255), random(255), random(255));
       for(let i = 1 ; i < 17 ; i++){
-        ellipse(result[key][3*i], result[key][3*i+1], 10, 10);
+        ellipse(parseInt(result[key][3*i]), parseInt(result[key][3*i+1]), 10, 10);
       }
       
        for(let i = 0 ; i < 19 ; i++){
-            line(result[key][3 * skeleton[i][0]], result[key][3 * skeleton[i][0] + 1],
-            result[key][3 * skeleton[i][1]], result[key][3 * skeleton[i][1] + 1]);
+            line(parseInt(result[key][3 * skeleton[i][0]]), parseInt(result[key][3 * skeleton[i][0] + 1]),
+            parseInt(result[key][3 * skeleton[i][1]]), parseInt(result[key][3 * skeleton[i][1] + 1]));
        }
     }
     
