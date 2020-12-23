3f9b79bed8b1efed58e168394a379691a0a6a33a
yeseuljoo <dptmfe93@naver.com>
2020-12-23 09:49:00 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -22,7 +22,7 @@ function preload() {
     console.log(request.response);
     let result = JSON.parse(request.response);
     
-    foreach(key in result.keys){
+    foreach(key in Object.keys(result)){
       console.log(result[key].keypoints);
     }
     
