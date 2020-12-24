1e8b3f113ceba3c01747605dc5328c64448e8b52
yeseuljoo <dptmfe93@naver.com>
2020-12-24 15:05:03 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -26,7 +26,8 @@ function preload() {
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   request.setRequestHeader('Authorization', 'KakaoAK 687ea12e4ef2be02334d085696877d60');
   
-  request.send('image_url='+ encodeURI('https://jooyesle.github.io/assets/people.jpg'));
+  //request.send('image_url='+ encodeURI('https://jooyesle.github.io/assets/people.jpg'));
+  request.send('image_url='+ encodeURI('https://i.ibb.co/tQqJnV4/friends.jpg'));
   
   request.onload = function() {
     sw = true;
