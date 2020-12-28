32419b080c3c69fa2d89d547758efb0326d17ad5
yeseuljoo <dptmfe93@naver.com>
2020-12-28 10:29:17 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -78,16 +78,12 @@ function mousePressed() {
     req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     //req.setRequestHeader('Content-Transfer-Encoding', 'base64');
   
-    //req.setRequestHeader('Access-Control-Allow-Origin', '*');
-    //req.setRequestHeader('Access-Control-Allow-Methods', 'POST'); 
-  
     saveimg.loadPixels();
     //req.send('image='+ capture.canvas.toDataURL("image/png").split(';base64,')[1]);
-    //req.send('image=iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==');
+    req.send('image=iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==');
     
-    req.send('image='+saveimg.canvas.toDataURL("image/png").split(';base64,')[1]);
-  
-    console.log('image='+saveimg.canvas.toDataURL("image/png").split(';base64,')[1]);
+    //req.send('image='+saveimg.canvas.toDataURL("image/png").split(';base64,')[1]);
+    //console.log('image='+saveimg.canvas.toDataURL("image/png").split(';base64,')[1]);
     
     req.onload = function() {
     console.log(req.response);
