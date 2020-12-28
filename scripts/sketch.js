dd7a417e388db093ad697864adba30fae32be443
yeseuljoo <dptmfe93@naver.com>
2020-12-28 13:43:29 +0900

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -76,17 +76,13 @@ function mousePressed() {
     req.setRequestHeader('Access-Control-Allow-Origin', '*');
     req.setRequestHeader('Access-Control-Allow-Methods', 'POST');
     req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    //req.setRequestHeader('Content-Transfer-Encoding', 'base64');
-  
-    //req.send('image=iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==');
-  
+
     saveimg.loadPixels();
     let b64str = saveimg.canvas.toDataURL("image/png").split(';base64,')[1];
     let sendstr = b64str.replaceAll('+', '%2B')
     req.send('image=' + sendstr);
-    console.log('image=' + sendstr);
     
     req.onload = function() {
-    console.log(req.response);
+    console.log(req.response.url);
   }
 }
