b6358d21defa4473a812509202a915155af6342d
yeseuljoo <dptmfe93@naver.com>
2021-01-04 10:02:31 +0900

    Create simple.js

---
+++ scripts/simple.js
@@ -0,0 +1,25 @@
+
+let x, y;
+
+function setup() {
+  createCanvas(720, 400);
+
+  x = width / 2;
+  y = height;
+}
+
+function draw() {
+  background(200);
+  
+  stroke(50);
+  fill(100);
+  ellipse(x, y, 24, 24);
+
+  x = x + random(-1, 1);
+  y = y - 1;
+  
+  if (y < 0) {
+    y = height;
+  }
+}
+
